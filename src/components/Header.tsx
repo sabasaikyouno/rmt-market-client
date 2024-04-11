dcf5f819be617a372effce1ed89685dcd6b7318c
sabasaikyouno <0852kuro@gmail.com>
2024-04-11 17:16:28 +0900

    Headerの検索作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Autocomplete, Grid, TextField, Toolbar, Typography } from "@mui/material";
+
+export default function Header({ searchOptions }) {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Typography variant="h3">rmt-market</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Autocomplete
+              options={searchOptions}
+              renderInput={(params) => <TextField {...params} label="Search" />}
+            />
+          </Grid>
+          <Grid item xs></Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+const serachTitle = [{
+  label: "a"
+}]
